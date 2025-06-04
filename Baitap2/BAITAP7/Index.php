e5d0d9710a7b389dea021b2575f6d5a6eacefc90
maimacma <110751930+maimacma@users.noreply.github.com>
2025-06-04 09:37:56 +0000

    thembai

---
+++ Baitap2/BAITAP7/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Input Example</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <label for="userInput">Enter something:</label>
+        <input type="text" id="userInput" name="userInput">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
