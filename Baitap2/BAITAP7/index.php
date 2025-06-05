07db0e67cdddda565303f638faf7b86114a1f652
maimacma <110751930+maimacma@users.noreply.github.com>
2025-06-05 17:15:18 +0000

    cải thiện vòng lặp hiển thị tên file và thêm tùy chọn cho danh sách ảnh

--- Baitap2/BAITAP7/index.php
+++ Baitap2/BAITAP7/index.php
@@ -9,8 +9,8 @@
     <form action="#" method="post">
   <label for="fruit">Chọn ảnh:</label>
 <select id="fruit" name="fruit">
-<option value="apple">Apple</option>
-</select>
+
+
 <?php 
 $diachi = ".";
 $mafile = ["jpg", "png", "psd", "JPG", "PNG", "PSD"];
@@ -18,10 +18,15 @@ $tenfile = scandir($diachi);
 $bofile = array_diff($tenfile , array('.', '..'));
 foreach($bofile as $bofiles)
 {
-   $mor = pathinfo($tenfiles,PATHINFO_EXTENSION); 
-echo $bofiles."<br>";
+   $mor = pathinfo($tenfiles,PATHINFO_EXTENSION);
+   if(array_search($mor,$mafile) !== false)
+   {
+       echo '<option value="'.$bofiles.'">Apple</option>';
+   }
+
 }
 ?>
+</select>
     </form>
 </body>
 </html>
