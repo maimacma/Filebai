0f37e50f7f0777b4249a836435cd367c9e7aed0b
maimacma <110751930+maimacma@users.noreply.github.com>
2025-07-01 02:29:25 +0000

    Add session handling and fetch department data from database

--- Baitap2/BAITAP8/mapb.php
+++ Baitap2/BAITAP8/mapb.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
 $con = require_once 'connect.php';
+$mapb = 'select * from PHONGBAN';
+$result = mysqli_query($con, $mapb);
+$mang=[];
+while ($mang = $result->fetch_assoc())
+{
+    $mang[] = $mang;
+}
+$_SESSION = $mang;
+$_SESSION['mapb'] = $mang;
 ?>
