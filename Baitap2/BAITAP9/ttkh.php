b7e949fa3b13471d20ce87dbf9215cc7f776307b
maimacma <110751930+maimacma@users.noreply.github.com>
2025-07-14 08:14:27 +0000

    Add customer information table to ttkh.php

---
+++ Baitap2/BAITAP9/ttkh.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Thông Tin Khách Hàng</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 60%;
+            margin: 30px auto;
+        }
+        th, td {
+            border: 1px solid #333;
+            padding: 8px 12px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        h2 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h2>Bảng Thông Tin Khách Hàng</h2>
+    <table>
+        <tr>
+            <th>STT</th>
+            <th>Họ Tên</th>
+            <th>Email</th>
+            <th>Số Điện Thoại</th>
+            <th>Địa Chỉ</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Nguyễn Văn A</td>
+            <td>vana@gmail.com</td>
+            <td>0901234567</td>
+            <td>Hà Nội</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Trần Thị B</td>
+            <td>thib@gmail.com</td>
+            <td>0912345678</td>
+            <td>TP. Hồ Chí Minh</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Lê Văn C</td>
+            <td>vanc@gmail.com</td>
+            <td>0923456789</td>
+            <td>Đà Nẵng</td>
+        </tr>
+    </table>
+</body>
+</html>
